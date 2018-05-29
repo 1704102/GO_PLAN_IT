1bd493d12051f6110de2551e639877538d506afd
Sijmen <sijmen.jaarsma@student.hu.nl>
2018-05-29 14:12:28 +0200

    -added code for cleanUp

--- src/main/java/com/example/jersey/TaskPlannerAdd/Day.java
+++ src/main/java/com/example/jersey/TaskPlannerAdd/Day.java
@@ -1,17 +1,26 @@
 package com.example.jersey.TaskPlannerAdd;
 import java.util.ArrayList;
-
+import java.util.Calendar;
+import java.util.Date;
 public class Day {
     private int timeSpent=0;
     private int Dayscore=0;
     private ArrayList<Integer> Freehours= new ArrayList<Integer>();
-    private String Date;
-    public Day(){
+    private Date date;
+    Calendar c = Calendar.getInstance();
+    public Day(Date d, boolean lastDays){
+
+        if(lastDays==true) {
+            addscore(10);
+        }
+        if(d.getDay()==6 || d.getDay()==7){
+            addscore(7);
+        }
 
     }
 
 
-    //timespent+ standard score+ time into project+ random values.
+    //code for time block timespent+ standard score+ time into project+ random values.
 
     public void setTimeSpent(int x){
     timeSpent = x;

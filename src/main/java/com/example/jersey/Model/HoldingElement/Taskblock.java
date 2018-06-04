af07b14e3555c6cc221e4913358a9ff158b7649e
Sijmen <sijmen.jaarsma@student.hu.nl>
2018-06-04 10:33:36 +0200

    added taskblocks to deal with a problem

---
+++ src/main/java/com/example/jersey/Model/HoldingElement/Taskblock.java
@@ -0,0 +1,57 @@
+package com.example.jersey.Model.HoldingElement;
+
+import java.util.Date;
+
+public class Taskblock {
+    Date startTime;
+    Date endTime;
+    int duration;
+    String taskname;
+    String subtask;
+
+
+    Taskblock(Date Start, Date End, String taskname){
+
+    }
+
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getTaskname() {
+        return taskname;
+    }
+
+    public void setTaskname(String taskname) {
+        this.taskname = taskname;
+    }
+
+    public String getSubtask() {
+        return subtask;
+    }
+
+    public void setSubtask(String subtask) {
+        this.subtask = subtask;
+    }
+}

638f462bc358e5004d355543e72f0a91387aee95
mansooj001 <jayceonsan@gmail.com>
2018-06-05 14:55:45 +0200

    [ADDED] parse functions and Appointment POJO

--- src/main/java/com/example/jersey/Database/AppointmentDatabase.java
+++ src/main/java/com/example/jersey/Database/AppointmentDatabase.java
@@ -1,15 +1,20 @@
 package com.example.jersey.Database;
 
 import com.example.jersey.Controller.Controller;
+import com.example.jersey.Controller.Util;
+import com.example.jersey.Model.Appointment;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-public class AppointmentDatabase extends DatabaseHelper{
+public class AppointmentDatabase extends DatabaseHelper {
 
-    public void addAppointment(JSONObject appointment){
+    public void addAppointment(JSONObject appointment) {
         connect();
         try {
             if (appointment.getString("date").equals("")) {
@@ -29,7 +34,9 @@ public class AppointmentDatabase extends DatabaseHelper{
                 preparedStatement.setString(5, appointment.getString("date"));
                 preparedStatement.execute();
             }
-        }catch (Exception e){e.printStackTrace();}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         disconnect();
     }
 
@@ -39,9 +46,9 @@ public class AppointmentDatabase extends DatabaseHelper{
         connect();
         try {
             PreparedStatement preparedStatement = connection.prepareStatement("select * , DATE_FORMAT(timeB, '%H:%i') as `timeBAltered` , DATE_FORMAT(timeE, '%H:%i') as `timeEAltered` from appointment where user_id = ?");
-            preparedStatement.setInt(1,Controller.getUser(input.getString("token")).getId());
+            preparedStatement.setInt(1, Controller.getUser(input.getString("token")).getId());
             ResultSet s = preparedStatement.executeQuery();
-            while (s.next()){
+            while (s.next()) {
                 JSONObject object = new JSONObject();
                 object.put("id", s.getInt("id"));
                 object.put("name", s.getString("name"));
@@ -50,7 +57,7 @@ public class AppointmentDatabase extends DatabaseHelper{
                 object.put("timeE", s.getString("timeEAltered"));
                 array.put(object);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         disconnect();
@@ -59,13 +66,31 @@ public class AppointmentDatabase extends DatabaseHelper{
 
     public void deleteAppointment(int id) {
         connect();
-        try{
+        try {
             PreparedStatement preparedStatement = connection.prepareStatement("delete from appointment where id = ?");
-            preparedStatement.setInt(1,id);
+            preparedStatement.setInt(1, id);
             preparedStatement.execute();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         disconnect();
+    }
+
+    public Appointment parseJSonObject(JSONObject object) {
+        String name = object.getString("name");
+        String timeB = object.getString("timeB");
+        String timeE = object.getString("timeE");
+        String dateString = object.getString("date");
+        LocalDate date = Util.stringToDate(dateString);
+        return new Appointment(name, timeB, timeE, date);
+    }
+
+    public List<Appointment> parseJSONArrayList(JSONArray array) {
+        List<Appointment> appointments = new ArrayList<>();
+
+        for (Object object : array) {
+            appointments.add(parseJSonObject((JSONObject) object));
+        }
+        return appointments;
     }
 }
